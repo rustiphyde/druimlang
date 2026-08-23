c3ffe6a71136ca6746bced7c5dadd2e23d707e3f
Rusty Hoppins <rustiphyde@gmail.com>
2026-08-23 00:36:50 -0500

    Add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         content="width=device-width, initial-scale=1.0"
     >
 
-    <<link rel="icon" type="image/png" href="/favicon.png">
+    <link rel="icon" type="image/png" href="/favicon.png">
 
     <title>Druim Programming Language</title>
 
